e00d122e0785638aec46819fd59945e5a03e0b8a
wasupon <wasupon.mail@gmail.com>
2024-09-03 16:44:24 +0900

    feat: navbar with avatar and sign in button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Providers } from "@/app/providers";
+import Header from "@/components/header";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -18,7 +19,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers> {children} </Providers>
+        <div className="container mx-auto px-4 max-w-6xl">
+          <Providers>
+            <Header />
+            {children}
+          </Providers>
+        </div>
       </body>
     </html>
   );
---
+++ src/components/header.tsx
@@ -0,0 +1,76 @@
+import * as actions from "@/actions";
+import { auth } from "@/auth";
+import {
+  Avatar,
+  Button,
+  Input,
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@nextui-org/react";
+import Link from "next/link";
+
+export default async function Header() {
+  const session = await auth();
+
+  let authContent: React.ReactNode;
+
+  if (session?.user) {
+    authContent = (
+      <Popover placement="left">
+        <PopoverTrigger>
+          <Avatar className="cursor-pointer" src={session?.user.image || ""} />
+        </PopoverTrigger>
+        <PopoverContent>
+          <div className="p-4">
+            <form action={actions.signOut}>
+              <Button type="submit">Sign Out</Button>
+            </form>
+          </div>
+        </PopoverContent>
+      </Popover>
+    );
+  } else {
+    authContent = (
+      <>
+        <NavbarItem>
+          <form action={actions.signIn}>
+            <Button type="submit" color="secondary" variant="bordered">
+              Sign In
+            </Button>
+          </form>
+        </NavbarItem>
+
+        <NavbarItem>
+          <form action={actions.signIn}>
+            <Button type="submit" color="primary" variant="flat">
+              Sign Up
+            </Button>
+          </form>
+        </NavbarItem>
+      </>
+    );
+  }
+
+  return (
+    <Navbar className="shadow mb-6">
+      <NavbarBrand>
+        <Link href={"/"} className="font-bold">
+          Discuss
+        </Link>
+      </NavbarBrand>
+
+      <NavbarContent justify="center">
+        <NavbarItem>
+          <Input placeholder="Search" />
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarContent justify="end">{authContent}</NavbarContent>
+    </Navbar>
+  );
+}
