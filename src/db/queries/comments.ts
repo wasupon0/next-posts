a452067d10919a0662c78683793860fad30544f4
wasupon <wasupon.mail@gmail.com>
2024-09-04 16:51:31 +0900

    fix: memoization fetchCommentsByPostId

--- src/db/queries/comments.ts
+++ src/db/queries/comments.ts
@@ -1,23 +1,25 @@
 import { db } from "@/db";
 import type { Comment } from "@prisma/client";
+import { cache } from "react";
 
 export type CommentWithAuthor = Comment & {
   user: { name: string | null; image: string | null };
 };
 
-export function fetchCommentsByPostId(
-  postId: string
-): Promise<CommentWithAuthor[]> {
-  console.log("fetchCommentsByPostId query!");
-  return db.comment.findMany({
-    where: { postId },
-    include: {
-      user: {
-        select: {
-          name: true,
-          image: true,
+export const fetchCommentsByPostId = cache(
+  (postId: string): Promise<CommentWithAuthor[]> => {
+    console.log("fetchCommentsByPostId query!");
+
+    return db.comment.findMany({
+      where: { postId },
+      include: {
+        user: {
+          select: {
+            name: true,
+            image: true,
+          },
         },
       },
-    },
-  });
-}
+    });
+  }
+);
