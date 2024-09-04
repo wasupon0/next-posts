37064ef1af6f355993ad888db65d81a9128e7d5c
wasupon <wasupon.mail@gmail.com>
2024-09-04 16:34:29 +0900

    feat: comments components logic

---
+++ src/db/queries/comments.ts
@@ -0,0 +1,22 @@
+import { db } from "@/db";
+import type { Comment } from "@prisma/client";
+
+export type CommentWithAuthor = Comment & {
+  user: { name: string | null; image: string | null };
+};
+
+export function fetchCommentsByPostId(
+  postId: string
+): Promise<CommentWithAuthor[]> {
+  return db.comment.findMany({
+    where: { postId },
+    include: {
+      user: {
+        select: {
+          name: true,
+          image: true,
+        },
+      },
+    },
+  });
+}
---
+++ src/db/queries/posts.ts
@@ -0,0 +1,19 @@
+import { db } from "@/db";
+import type { Post } from "@prisma/client";
+
+export type PostWithData = Post & {
+  topic: { slug: string };
+  user: { name: string | null };
+  _count: { comments: number };
+};
+
+export function fetchPostsByTopicSlug(slug: string): Promise<PostWithData[]> {
+  return db.post.findMany({
+    where: { topic: { slug } },
+    include: {
+      topic: { select: { slug: true } },
+      user: { select: { name: true } },
+      _count: { select: { comments: true } },
+    },
+  });
+}
